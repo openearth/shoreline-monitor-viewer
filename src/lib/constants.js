2b5816c8cb143e1c469d10396d3202108ba8e532
Robert <robertbroersma@gmail.com>
2025-11-06 15:36:18 +0100

    Swap satellite style with new one including labels

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -4,7 +4,7 @@ export const MAP_ZOOM = 10.5
 export const MAP_BASELAYERS = [
   { title: 'Light', uri: 'mapbox://styles/mapbox/light-v11' },
   { title: 'Dark', uri: 'mapbox://styles/mapbox/dark-v11' },
-  { title: 'Satellite', uri: 'mapbox://styles/mapbox/satellite-v9' },
+  { title: 'Satellite', uri: 'mapbox://styles/mapbox/standard-satellite' },
 ]
 
 export const MAP_BASELAYER_DEFAULT = MAP_BASELAYERS[0]
