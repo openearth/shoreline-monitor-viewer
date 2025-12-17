b89c1f14ff5164ea47c5e3c0f6685e8ff164e55b
luisrodriguezgalvez <77997772+luisrodriguezgalvez@users.noreply.github.com>
2025-12-17 17:53:52 +0100

    map in left half of screen

--- src/views/AdvancedUser.vue
+++ src/views/AdvancedUser.vue
@@ -1,9 +1,34 @@
 <template>
-  <div>
-    <!-- Advanced user view content will go here -->
-  </div>
+  <v-container fluid class="pa-0 full-height">
+    <v-row no-gutters class="full-height">
+      <v-col cols="12" md="6" class="left-pane">
+        <map-component />
+      </v-col>
+      <v-col cols="12" md="6" class="right-pane">
+        <!-- Advanced tools will be added here -->
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script setup>
-  //
+  import { onMounted } from 'vue'
+  import { useMapStore } from '@/stores/map'
+
+  const mapStore = useMapStore()
+
+  onMounted(() => {
+    mapStore.initializeMapboxLayers()
+  })
 </script>
+
+<style scoped>
+.full-height {
+  height: calc(100vh - 64px);
+}
+
+.left-pane,
+.right-pane {
+  height: 100%;
+}
+</style>
