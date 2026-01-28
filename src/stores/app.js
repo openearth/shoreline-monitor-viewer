d61a6345c02dfc7307c8f8e84aaefbb432550225
luisrodriguezgalvez <77997772+luisrodriguezgalvez@users.noreply.github.com>
2026-01-28 13:34:51 +0100

    legal dialog once per session

--- src/stores/app.js
+++ src/stores/app.js
@@ -4,11 +4,16 @@ import { defineStore } from 'pinia'
 export const useAppStore = defineStore('app', {
   state: () => ({
     panelIsCollapsed: true,
-    acceptedLegal: false,
+    acceptedLegal: typeof window === 'undefined'
+      ? false
+      : window.localStorage.getItem('acceptedLegal') === 'true',
   }),
   actions: {
     acceptLegal () {
       this.acceptedLegal = true
+      if (typeof window !== 'undefined') {
+        window.localStorage.setItem('acceptedLegal', 'true')
+      }
     },
     collapsePanel () {
       this.panelIsCollapsed = true
